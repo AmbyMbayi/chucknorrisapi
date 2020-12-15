92c8a2710556c25a8cf2e87342c679cd78de0cb4
AmbyMbayi <ambymbayi54@gmail.com>
2020-12-15 21:08:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import React, { useState } from "react";
 import "./App.css";
+import axios from 'axios'
 
-import { fetchCategories, fetchCategoryJoke } from "./api";
+import { Button, Modal, Row, Col, Container } from "react-bootstrap";
+
 
 function App() {
-  const [jokes, setJokes] = useState(null);
+
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	const [categories, setCategories] = useState(null)
+
+	const apiURL  ="https://api.chucknorris.io/jokes/categories"
+
+	const fetchData = async () => {
+		const response = await axios.get(apiURL)
+
+		setCategories(response.data)
+		
+		
+	}
+
+
 
   return (
-    <div className="App">
-      <h1 onClick={fetchCategoryJoke}>Chucknorris APP</h1>
-
-      <div className="jokes">
-        {jokes &&
-          jokes.map((joke, index) => {
-            return (
-              <div>
-                <a href="">{joke}</a>
-              </div>
-            );
-          })}
+    <div className="container mt-4 mb-4">
+      <div className="mt-3">
+        <h3>Random Chuck Norris Jokes</h3>
+        <Button variant="primary" onClick={fetchData} className="mt-3">
+              Get categories
+            </Button>
       </div>
+      <Container>
+        <Row>
+        {categories && 
+        	categories.map((category, index) => {
+        	return(
+        	 <Col sm={4} key={index}>
+            <Button variant="primary" onClick={handleShow} className="mt-2 p-3 button ">
+              {category}
+            </Button>
+          </Col>
+        	)
+        })}
+         
+        </Row>
+      </Container>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header >
+          <Modal.Title>Category Name</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Category fact</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+         
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,15 @@ const apiURL = "https://api.chucknorris.io/jokes/categories";
 
 export const fetchCategories = async () => {
   const { data } = await axios.get(apiURL);
-  console.log(data);
+ console.log(data)
   return data;
 };
+
+	
+/*const getRandomCategoryJoke = (category) =>_{
+  request.get(`${baseURl}random?category=${category}`);
+}
+
+const getJokesCategories = () => {
+	request.get(`${baseURl}categories`);
+}*/
